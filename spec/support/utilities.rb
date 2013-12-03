fdb1db565dcd1523970856dc67cdd32afb8a1292
tRav3r <tRav3r@gmail.com>
2013-12-03 16:54:24 +0200

    Finish user edit, update, index, and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -11,3 +11,12 @@ RSpec::Matchers.define :have_error_message do |message|
   	page.shoul have_selector('div.alert.alert-error', text: message)
   end
 end
+
+def sign_in(user)
+  visit signin_path
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  # Вход без Capybara.
+  cookies[:remember_token] = user.remember_token
+end
